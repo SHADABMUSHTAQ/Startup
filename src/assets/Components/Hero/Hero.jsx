b349f0ba3670b5ef24e183f87cfb9275bfd3e8a8
Muhammad Shadab <shadabmushtaq44@gmail.com>
2026-04-01 13:15:06 +0500

    Fresh frontend code for main branch

--- src/assets/Components/Hero/Hero.jsx
+++ src/assets/Components/Hero/Hero.jsx
@@ -57,7 +57,7 @@ function Hero() {
         {/* Right Side: Image/Visual */}
         <div className="hero-visual">
           <div className="image-backdrop"></div>
-          <img src="/hero-img.png" alt="WarSOC Dashboard Preview" className="floating-img" />
+          <img src="public/Logo.png" alt="WarSOC Dashboard Preview" className="floating-img" />
         </div>
 
       </div>
